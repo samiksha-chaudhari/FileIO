7fb26215fbeac9bf62f556146be05b3415cab8af
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-10-07 00:33:12 +0530

    performing different file IO operations

--- FileIO_Demo/Program.cs
+++ FileIO_Demo/Program.cs
@@ -1,12 +1,80 @@
 ﻿using System;
+using System.IO;
 
 namespace FileIO_Demo
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to FileIO demo");
+            //Program.FileExists();
+            //Program.ReadAllLines();
+            //Program.ReadAllText();
+            //Program.FileCopy();
+            //Program.DeleteFile();
+            Read readstream = new Read();
+            //readstream.WriteUsingStreamWriter();
+            readstream.ReadFromStreamReader();
+        }
+
+        /// <summary>
+        /// method to chek file exits at the path or not
+        /// </summary>
+        public static void FileExists()
+        {
+            String path = @"D:\new1\bridgelabz_fellowship\FileIO_Demo\FileIO_Demo\Example.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File exists at given path");
+            }
+        }
+
+        /// <summary>
+        /// method to read file text by line
+        /// </summary>
+        public static void ReadAllLines()
+        {
+            String path = @"D:\new1\bridgelabz_fellowship\FileIO_Demo\FileIO_Demo\Example.txt";
+            String[] lines;
+            lines = File.ReadAllLines(path);
+            Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
+            Console.WriteLine(lines[2]);
+            Console.WriteLine(lines[3]);
+        }
+
+        /// <summary>
+        /// method to read all text from file
+        /// </summary>
+        public static void ReadAllText()
+        {
+            String path = @"D:\new1\bridgelabz_fellowship\FileIO_Demo\FileIO_Demo\Example.txt";
+            String lines;
+            lines = File.ReadAllText(path);
+            Console.WriteLine(lines);
+
+        }
+        /// <summary>
+        /// method to copy one file text to other file
+        /// </summary>
+        public static void FileCopy()
+        {
+            string path = @"D:\new1\bridgelabz_fellowship\FileIO_Demo\FileIO_Demo\Example.txt";
+            string copypath = @"D:\new1\bridgelabz_fellowship\FileIO_Demo\FileIO_Demo\Example1.txt";
+
+            File.Copy(path, copypath);
+
+        }
+        /// <summary>
+        /// method to delete file from given path 
+        /// </summary>
+        public static void DeleteFile()
+        {
+            string path = @"D:\new1\bridgelabz_fellowship\FileIO_Demo\FileIO_Demo\Example1.txt";
+
+            File.Delete(path);
+
         }
     }
 }
---
+++ FileIO_Demo/Read.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace FileIO_Demo
+{
+    class Read
+    {
+        //By using strem reader inbuild class
+
+        /// <summary>
+        /// method to write or Append text in existing file
+        /// </summary>
+        public void WriteUsingStreamWriter()
+        {
+            String path = @"D:\new1\bridgelabz_fellowship\FileIO_Demo\FileIO_Demo\Example.txt";
+
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine("\n Hello everyone.. My name is Samiksha");
+                sr.Close();
+                Console.WriteLine(File.ReadAllText(path));
+            }
+        }
+
+        /// <summary>
+        /// method to read the line from file
+        /// </summary>
+        public void ReadFromStreamReader()
+        {
+            String path = @"D:\new1\bridgelabz_fellowship\FileIO_Demo\FileIO_Demo\Example.txt";
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                String s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
