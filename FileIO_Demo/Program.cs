095911645a9cb79e0a01aa04eb74d5cae1ce12db
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-10-06 23:37:18 +0530

    welcome

---
+++ FileIO_Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileIO_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to FileIO demo");
+        }
+    }
+}
